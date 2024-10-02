2435074c7bcdd045c282fd2b5a9e0c49c68f1c3d
cuongtq1026 <cuongtq1026@gmail.com>
2024-10-02 13:05:27 +0700

    feat: upgrade logger

--- services/monitor/logger.ts
+++ services/monitor/logger.ts
@@ -24,15 +24,38 @@ if (process.env.NODE_ENV !== "production") {
     new transports.Console({
       level: "debug",
       format: format.combine(
-        format.colorize({ all: true }),
         format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
-        format.printf(({ level, message, timestamp, ...metadata }) => {
-          let msg = `${timestamp} ${level} ${message}`;
-          if (metadata.file) {
-            msg += ` [${metadata.file}:${metadata.line}]`;
-          }
-          return msg;
-        }),
+        format.printf(
+          ({
+            level,
+            message,
+            label,
+            namespace,
+            timestamp,
+            file,
+            ...metadata
+          }) => {
+            const messages = [];
+            messages.push(timestamp);
+            if (label) {
+              messages.push(`[${label}]`);
+            }
+            messages.push(level.toUpperCase());
+            if (namespace) {
+              messages.push(namespace);
+            }
+            messages.push(message);
+            if (file) {
+              messages.push(`[${metadata.file}:${metadata.line}]`);
+            }
+            const otherMessage = JSON.stringify(metadata);
+            if (otherMessage && otherMessage !== "{}") {
+              messages.push(otherMessage);
+            }
+            return messages.join(" ");
+          },
+        ),
+        format.colorize({ all: true }),
       ),
     }),
   );

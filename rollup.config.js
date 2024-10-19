f9d738be3472050828cc5a1df5a91c6d7ce7c025
cuongtq1026 <cuongtq1026@gmail.com>
2024-10-19 11:17:39 +0700

    feat: remove rollup config

--- rollup.config.js
+++
@@ -1,8 +0,0 @@
-import { extendConfig } from "pkgroll";
-import { string } from "rollup-plugin-string";
-
-export default extendConfig({
-  plugins: [
-    string({ include: "**/*.avsc" }), // This will import .avsc files as raw text
-  ],
-});
